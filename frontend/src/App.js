b09703cb351c19021d40f2b0f3a655f26ec1ed2c
tpatel1781 <tpatel1781@gmail.com>
2018-12-24 12:13:24 -0500

    Fixed comma bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   state = {
-    response: '',
-    post: '',
-    responseToPost: '',
+    response: "",
+    post: "",
+    responseToPost: ""
   };
   componentDidMount() {
     this.callApi()
@@ -14,30 +14,30 @@ class App extends Component {
       .catch(err => console.log(err));
   }
   callApi = async () => {
-    const response = await fetch('/api/hello');
+    const response = await fetch("/api/hello");
     const body = await response.json();
     if (response.status !== 200) throw Error(body.message);
     return body;
   };
   handleSubmit = async e => {
     e.preventDefault();
-    const response = await fetch('/api/world', {
-      method: 'POST',
+    const response = await fetch("/api/world", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json"
       },
-      body: JSON.stringify({ post: this.state.post }),
+      body: JSON.stringify({ post: this.state.post })
     });
     const body = await response.text();
     this.setState({ responseToPost: body });
   };
-render() {
+  render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload and test.
           </p>
           <a
             className="App-link"
